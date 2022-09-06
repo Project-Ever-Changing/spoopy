7ee071f0406e66b432f698db9b2540e5d074238a
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-09-06 15:46:36 -0400

    new init

--- src/spoopy/ApplicationPlugin.hx
+++ src/spoopy/ApplicationPlugin.hx
@@ -1,9 +1,10 @@
 package spoopy;
 
+import spoopy.backend.SpoopyCFFI;
 import lime.app.Application;
 
 class ApplicationPlugin {
     public static inline function init(app:Application):Void {
-        
+        SpoopyCFFI.spoopy_application_init();
     }
 }
