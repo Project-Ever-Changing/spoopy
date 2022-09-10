e77eb5f209dbf2e46f3ce29efbee41f73f0adfd4
Diego <fonseca.diego@outlook.com>
2022-09-09 22:34:07 -0400

    again

--- scripts/RunScript.hx
+++ scripts/RunScript.hx
@@ -9,14 +9,14 @@ class RunScript {
         var cwd = args.pop();
 
         var shellScripts:Array<String> = [
-            "/shell/build.sh",
-            "/shell/compile.sh"
+            "build.sh",
+            "compile.sh"
         ];
 
         for(i in 0...shellScripts.length) {
             try {
                 trace(Sys.getCwd() + shellScripts[i]);
-                Sys.setCwd("sh " + Sys.getCwd() + shellScripts[i]);
+                Sys.setCwd("sh " + Sys.getCwd() + "scripts/shell/" + shellScripts[i]);
             }catch(e:Dynamic) {
                 Log.error("Could not find shellscript: " + Sys.getCwd() + shellScripts[i]);
             }
