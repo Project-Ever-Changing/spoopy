0d0aa09a35f93f8f3fe7fec09d237c49b0f1bbb1
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-10-03 11:26:27 -0400

    OMG HAHA

--- project/src/device/WindowDevice.cpp
+++ project/src/device/WindowDevice.cpp
@@ -6,7 +6,7 @@ namespace spoopy {
     #ifdef SPOOPY_VULKAN
     void WindowDevice::createWindowSurfaceVulkan(VkInstance instance, VkSurfaceKHR* surface) {
         if(!SDL_Vulkan_CreateSurface(sdlWindow, instance, surface)) {
-            //throw std::runtime_error("Failed to create Window Surface for Vulkan API!");
+            throw std::runtime_error("Failed to create Window Surface for Vulkan API!");
         }else {
             
         }
