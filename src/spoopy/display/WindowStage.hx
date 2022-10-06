a573f6458ed81d97ddc4f7f6f9cd6c993cdd4a4d
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-10-06 11:28:26 -0400

    oops

--- src/spoopy/display/WindowStage.hx
+++ src/spoopy/display/WindowStage.hx
@@ -64,7 +64,7 @@ class WindowStage {
     }
 
     @:noCompletion private function set_parent(window:Window):Window {
-        if(applyWindowSurface) {
+        if(applyWindowSurface && __renderer != null) {
             __renderer.applyWindowSurface(device);
         }
 
