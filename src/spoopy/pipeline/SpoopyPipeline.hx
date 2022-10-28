1322b52a4e9bb42531e160f06b86ac94247feac7
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2022-10-28 12:00:47 -0400

    hmmmmmmmm

--- src/spoopy/pipeline/SpoopyPipeline.hx
+++ src/spoopy/pipeline/SpoopyPipeline.hx
@@ -1,9 +1,11 @@
 package pipeline;
 
+import backend.GraphicsPipline;
 import backend.DevicesPipeline;
 
 class SpoopyPipeline {
-    @:noCompletion var __manager:DevicesPipeline;
+    @:noCompletion var __devices:DevicesPipeline;
+    @:noCompletion var __graphics:GraphicsPipline;
 
     public function new() {
         __manager = new DevicesPipeline();
