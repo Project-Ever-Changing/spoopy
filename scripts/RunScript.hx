bba997e2c204baa07db4d39c78e1f9e751de3d54
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2022-11-10 11:41:50 -0500

    import

--- scripts/RunScript.hx
+++ scripts/RunScript.hx
@@ -19,162 +19,7 @@ class RunScript {
     }
 
     static inline function commands(args:Array<String>):Void {
-        if (args.length > 0 && args[0] == "setup") {
-            var limeDirectory = Haxelib.getPath(new Haxelib("lime"));
-
-            if (limeDirectory == null || limeDirectory == "" || limeDirectory.indexOf("is not installed") > -1) {
-                Sys.command("haxelib", ["install", "lime"]);
-            }
-
-            @:final var answer:Bool = askYN("Do you want to setup the command alias?");
-
-            if(!answer) {
-                return;
-            }
-            
-            if(FileSys.isWindows) {
-                var haxePath:String = Sys.getEnv("HAXEPATH");
-
-				if (haxePath == null || haxePath == "") {
-					haxePath = "C:\\HaxeToolkit\\haxe\\";
-                }
-
-                @:final var batchFile:String = "spoopy.bat";
-
-                var scriptSourcePath = Sys.getCwd();
-                scriptSourcePath = PathUtils.combine(scriptSourcePath, "bin");
-				scriptSourcePath = PathUtils.combine(scriptSourcePath, batchFile);
-
-                if(FileSystem.exists(scriptSourcePath)) {
-                    File.copy(scriptSourcePath, haxePath + "\\" + batchFile);
-                }else {
-                    Log.error("Could not find the spoopy alias script. You can try 'haxelib selfupdate' and run setup again.");
-                }
-            }else {
-                @:final var binPath = if (FileSys.isMac) "/usr/local/bin" else "/usr/bin";
-                
-                var shellScript = Sys.getCwd();
-                shellScript = PathUtils.combine(shellScript, "bin");
-                shellScript = PathUtils.combine(shellScript, "spoopy.sh");
-
-				if (FileSystem.exists(shellScript)) {
-					Sys.command("sudo", ["cp", shellScript, binPath + "/spoopy"]);
-					Sys.command("sudo", ["chmod", "+x", binPath + "/spoopy"]);
-				}
-				else {
-					Log.error("Could not find the spoopy alias script. You can try 'spoopy selfupdate' and run setup again.");
-				}
-            }
-        }else if(args[0] == "build") {
-            if(args.length <= 1) {
-                Log.error("Incorrect number of arguments for command 'build'");
-                return;
-            }
-
-            @:final var fileLocation:String = "scripts/";
-            @:final var libraries:Map<String, String> = [
-                "sdl" => "https://github.com/native-toolkit/libsdl",
-                "hashlink" => "https://github.com/HaxeFoundation/hashlink.git"
-            ];
-
-            var scripts:Array<String> = [];
-
-            if(FileSys.isWindows) {
-                scripts = [
-                    "batch/build.bat"
-                ];
-            }else {
-                scripts = [
-                    "shell/build.sh"
-                ];
-            }
-
-            if(!FileSystem.exists(Sys.getCwd() + "ndll")) {
-                FileSystem.createDirectory(Sys.getCwd() + "ndll");
-            }
-
-            Sys.command("git", ["submodule", "init"]);
-            Sys.command("git", ["submodule", "update"]);
-
-            switch(args[1].toLowerCase()) {
-                case "Windows" | "Windows64":
-                    for(i in 0...scripts.length) {
-                        if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
-                            Sys.command(Sys.getCwd() + fileLocation + scripts[i]);
-                        }else {
-                            Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
-                        }
-                    }
-                default:
-                    for(i in 0...scripts.length) {
-                        if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
-                            Sys.command("sh", [Sys.getCwd() + fileLocation + scripts[i]]);
-                        }else {
-                            Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
-                        }
-                    }
-            }
-        }else if(args[0] == "ls") {
-            if(args.length > 1) {
-                Sys.command("ls", [Sys.getCwd() + args[1]]);
-            }else {
-                Sys.command("ls");
-            }
-        }else if(args[0] == "destroy") {
-            if(args.length <= 1) {
-                Log.error("Incorrect number of arguments for command 'destroy'");
-                return;
-            }
-
-            @:final var fileLocation:String = "scripts/";
-
-            var scripts:Array<String> = [];
-
-            if(!FileSystem.exists(Sys.getCwd() + "ndll")) {
-                return;
-            }
-
-            if(FileSys.isWindows) {
-                scripts = [
-                    "batch/destroy.bat"
-                ];
-            }else {
-                scripts = [
-                    "shell/destroy.sh"
-                ];
-            }
-
-            switch(args[1].toLowerCase()) {
-                case "Windows" | "Windows64":
-                    for(i in 0...scripts.length) {
-                        if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
-                            Sys.command(Sys.getCwd() + fileLocation + scripts[i]);
-                        }else {
-                            Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
-                        }
-                    }
-                default:
-                    for(i in 0...scripts.length) {
-                        if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
-                            Sys.command("sh", [Sys.getCwd() + fileLocation + scripts[i]]);
-                        }else {
-                            Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
-                        }
-                    }
-            }
-        }else if(args[0] == "update") {
-            if(args.length <= 1) {
-                Log.error("Incorrect number of arguments for command 'update'");
-                return;
-            }
-
-            Sys.command("haxelib", ["update", "spoopy"]);
-            
-            commands(["destroy", args[1]]);
-            commands(["build", args[1]]);
-        }else if(args.length > 0) {
-            Log.error("Invalid command: '" + args[0] + "'");
-        }else {
+        if(args.length == 0) {
             if(FileSys.isWindows) {
                 @:final var displayScript:String = "scripts/batch/display.bat";
 
@@ -192,6 +37,23 @@ class RunScript {
                     Log.error("Could not find script: " + Sys.getCwd() + displayScript);
                 }
             }
+
+            return;
+        }
+
+        switch(args[0]) {
+            case "setup":
+                setupCMD(args);
+            case "build":
+                buildCMD(args);
+            case "ls":
+                lsCMD(args);
+            case "destroy":
+                destroyCMD(args);
+            case "update":
+                updateCMD(args);
+            default:
+                Log.error("Invalid command: '" + args[0] + "'");
         }
     }
 
@@ -214,6 +76,174 @@ class RunScript {
         return null;
     }
 
+    static inline function setupCMD(args:Array<String>):Void {
+        if(args.length == 0) {
+            return;
+        }
+
+        var limeDirectory = Haxelib.getPath(new Haxelib("lime"));
+
+        if (limeDirectory == null || limeDirectory == "" || limeDirectory.indexOf("is not installed") > -1) {
+            Sys.command("haxelib", ["install", "lime"]);
+        }
+
+        @:final var answer:Bool = askYN("Do you want to setup the command alias?");
+
+        if(!answer) {
+            return;
+        }
+        
+        if(FileSys.isWindows) {
+            var haxePath:String = Sys.getEnv("HAXEPATH");
+
+            if (haxePath == null || haxePath == "") {
+                haxePath = "C:\\HaxeToolkit\\haxe\\";
+            }
+
+            @:final var batchFile:String = "spoopy.bat";
+
+            var scriptSourcePath = Sys.getCwd();
+            scriptSourcePath = PathUtils.combine(scriptSourcePath, "bin");
+            scriptSourcePath = PathUtils.combine(scriptSourcePath, batchFile);
+
+            if(FileSystem.exists(scriptSourcePath)) {
+                File.copy(scriptSourcePath, haxePath + "\\" + batchFile);
+            }else {
+                Log.error("Could not find the spoopy alias script. You can try 'haxelib selfupdate' and run setup again.");
+            }
+        }else {
+            @:final var binPath = if (FileSys.isMac) "/usr/local/bin" else "/usr/bin";
+            
+            var shellScript = Sys.getCwd();
+            shellScript = PathUtils.combine(shellScript, "bin");
+            shellScript = PathUtils.combine(shellScript, "spoopy.sh");
+
+            if (FileSystem.exists(shellScript)) {
+                Sys.command("sudo", ["cp", shellScript, binPath + "/spoopy"]);
+                Sys.command("sudo", ["chmod", "+x", binPath + "/spoopy"]);
+            }
+            else {
+                Log.error("Could not find the spoopy alias script. You can try 'spoopy selfupdate' and run setup again.");
+            }
+        }
+    }
+
+    static inline function buildCMD(args:Array<String>):Void {
+        if(args.length <= 1) {
+            Log.error("Incorrect number of arguments for command 'build'");
+            return;
+        }
+
+        @:final var fileLocation:String = "scripts/";
+        @:final var libraries:Map<String, String> = [
+            "sdl" => "https://github.com/native-toolkit/libsdl",
+            "hashlink" => "https://github.com/HaxeFoundation/hashlink.git"
+        ];
+
+        var scripts:Array<String> = [];
+
+        if(FileSys.isWindows) {
+            scripts = [
+                "batch/build.bat"
+            ];
+        }else {
+            scripts = [
+                "shell/build.sh"
+            ];
+        }
+
+        if(!FileSystem.exists(Sys.getCwd() + "ndll")) {
+            FileSystem.createDirectory(Sys.getCwd() + "ndll");
+        }
+
+        Sys.command("git", ["submodule", "init"]);
+        Sys.command("git", ["submodule", "update"]);
+
+        switch(args[1].toLowerCase()) {
+            case "Windows" | "Windows64":
+                for(i in 0...scripts.length) {
+                    if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
+                        Sys.command(Sys.getCwd() + fileLocation + scripts[i]);
+                    }else {
+                        Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
+                    }
+                }
+            default:
+                for(i in 0...scripts.length) {
+                    if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
+                        Sys.command("sh", [Sys.getCwd() + fileLocation + scripts[i]]);
+                    }else {
+                        Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
+                    }
+                }
+        }
+    }
+
+    static inline function lsCMD(args:Array<String>):Void {
+        if(args.length > 1) {
+            Sys.command("ls", [Sys.getCwd() + args[1]]);
+        }else {
+            Sys.command("ls");
+        }
+    }
+
+    static inline function destroyCMD(args:Array<String>):Void {
+        if(args.length <= 1) {
+            Log.error("Incorrect number of arguments for command 'destroy'");
+            return;
+        }
+
+        @:final var fileLocation:String = "scripts/";
+
+        var scripts:Array<String> = [];
+
+        if(!FileSystem.exists(Sys.getCwd() + "ndll")) {
+            return;
+        }
+
+        if(FileSys.isWindows) {
+            scripts = [
+                "batch/destroy.bat"
+            ];
+        }else {
+            scripts = [
+                "shell/destroy.sh"
+            ];
+        }
+
+        switch(args[1].toLowerCase()) {
+            case "Windows" | "Windows64":
+                for(i in 0...scripts.length) {
+                    if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
+                        Sys.command(Sys.getCwd() + fileLocation + scripts[i]);
+                    }else {
+                        Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
+                    }
+                }
+            default:
+                for(i in 0...scripts.length) {
+                    if(FileSystem.exists(Sys.getCwd() + fileLocation + scripts[i])) {
+                        Sys.command("sh", [Sys.getCwd() + fileLocation + scripts[i]]);
+                    }else {
+                        Log.error("Could not find script: " + Sys.getCwd() + fileLocation + scripts[i]);
+                    }
+                }
+        }
+    }
+
+    static inline function updateCMD(args:Array<String>):Void {
+        if(args.length <= 1) {
+            Log.error("Incorrect number of arguments for command 'update'");
+            return;
+        }
+
+        Sys.command("haxelib", ["update", "spoopy"]);
+        
+        commands(["destroy", args[1]]);
+        commands(["build", args[1]]);
+    }
+
+    /*
     static inline function compileGraphics():Void {
         @:final var binPath = if (FileSys.isMac) "/usr/local/bin" else "/usr/bin";
         binPath += "/glslc";
@@ -232,4 +262,5 @@ class RunScript {
             Sys.command(binPath, ["shaders/VKGL/" + cacheDirectory[i], "-o", "shaders/VKGL/" + cacheDirectory[i] + ".spv"]);
         }
     }
+    */
 }
