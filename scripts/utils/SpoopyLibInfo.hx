fc8070bcdcd69e4e4d780b7f976b6d351e095bc0
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2022-12-09 16:12:41 -0500

    test

--- scripts/utils/SpoopyLibInfo.hx
+++ scripts/utils/SpoopyLibInfo.hx
@@ -2,6 +2,6 @@ package utils;
 
 class SpoopyLibInfo {
     @:final public static var commandName = "spoopy";
-	@:final public static var defaultLibrary = "spoopy";
+	@:final public static var defaultLibrary = "lime";
 	@:final public static var defaultLibraryName = "Spoopy Game";
 }
