b6dca5eb0f57448dc4b135aa477dab55b4d20532
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2022-12-09 16:02:51 -0500

    update

--- scripts/RunScript.hx
+++ scripts/RunScript.hx
@@ -260,7 +260,7 @@ class RunScript {
             return;
         }
 
-        CreateTemplate.createProject(["lime", args[1]], userDefines, null);
+        CreateTemplate.createProject(["project", args[1]], userDefines, null);
     }
 
     /*
