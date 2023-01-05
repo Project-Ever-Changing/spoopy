0601b1f76498b9023b21ad61c011c0fbbf797409
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-01-05 11:36:52 -0500

    oh no....

--- src/spoopy/backend/WindowRenderer.hx
+++ src/spoopy/backend/WindowRenderer.hx
@@ -19,8 +19,7 @@ class WindowRenderer {
     public function getWindowTitle(window:Window):String {
         #if (cpp && !cppia)
         if(window.__backend.handle != null) {
-            trace(window.__backend.handle.sdlWindow);
-            return SpoopyCFFI.spoopy_window_get_title(window.__backend.handle.sdlWindow);
+            return SpoopyCFFI.spoopy_window_get_title(window.__backend.handle);
         }
         #end
 
