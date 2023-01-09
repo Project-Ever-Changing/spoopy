3301d60a9f428891cdde3c7ce142bb5645ec6d18
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-01-08 19:37:40 -0500

    hmmm

--- project/src/sdl/SpoopySDLWindow.cpp
+++ project/src/sdl/SpoopySDLWindow.cpp
@@ -17,12 +17,12 @@ namespace spoopy {
 		*/
         int sdlFlags = 0;
 
-		if (flags & WINDOW_FLAG_FULLSCREEN) sdlFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
-		if (flags & WINDOW_FLAG_RESIZABLE) sdlFlags |= SDL_WINDOW_RESIZABLE;
-		if (flags & WINDOW_FLAG_BORDERLESS) sdlFlags |= SDL_WINDOW_BORDERLESS;
-		if (flags & WINDOW_FLAG_HIDDEN) sdlFlags |= SDL_WINDOW_HIDDEN;
-		if (flags & WINDOW_FLAG_MINIMIZED) sdlFlags |= SDL_WINDOW_MINIMIZED;
-		if (flags & WINDOW_FLAG_MAXIMIZED) sdlFlags |= SDL_WINDOW_MAXIMIZED;
+		if (flags & SDL_WINDOW_FULLSCREEN_DESKTOP) sdlFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+		if (flags & SDL_WINDOW_RESIZABLE) sdlFlags |= SDL_WINDOW_RESIZABLE;
+		if (flags & SDL_WINDOW_BORDERLESS) sdlFlags |= SDL_WINDOW_BORDERLESS;
+		if (flags & SDL_WINDOW_HIDDEN) sdlFlags |= SDL_WINDOW_HIDDEN;
+		if (flags & SDL_WINDOW_MINIMIZED) sdlFlags |= SDL_WINDOW_MINIMIZED;
+		if (flags & SDL_WINDOW_MAXIMIZED) sdlFlags |= SDL_WINDOW_MAXIMIZED;
 
         #ifndef EMSCRIPTEN
 		if (flags & WINDOW_FLAG_ALWAYS_ON_TOP) sdlFlags |= SDL_WINDOW_ALWAYS_ON_TOP;
