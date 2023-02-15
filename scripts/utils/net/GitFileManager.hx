9cb9ce65215b27c2d1c0861cdbed7dd3957db820
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-02-15 13:17:03 -0500

    receive data from the GitHub host

--- scripts/utils/net/GitFileManager.hx
+++ scripts/utils/net/GitFileManager.hx
@@ -3,6 +3,7 @@ package utils.net;
 import sys.net.Socket;
 import sys.net.Host;
 import sys.FileSystem;
+import hxp.Log;
 
 import lime.utils.Bytes as LimeBytes;
 
@@ -10,7 +11,12 @@ using StringTools;
 
 class GitFileManager {
     public static inline function download(url:String, downloadPath:String):Void {
-        var host = new Host(url.trim());
-        trace("Hostname: " + host.host);
+        var host = new Host("github.com");
+        
+        var gitSocket:Socket = new Socket();
+        gitSocket.connect(host, 443);
+        gitSocket.bind(host, 443);
+
+        Log.info(gitSocket.read());
     }
 }
