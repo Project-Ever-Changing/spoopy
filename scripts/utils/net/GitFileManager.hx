072ad337a51254b5b25d922f9d8e9e2ada8bfe3f
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-02-15 11:16:37 -0500

    Output URL to see if anything is wrong

--- scripts/utils/net/GitFileManager.hx
+++ scripts/utils/net/GitFileManager.hx
@@ -6,9 +6,13 @@ import sys.FileSystem;
 
 import lime.utils.Bytes as LimeBytes;
 
+using StringTools;
+
 class GitFileManager {
     public static inline function download(url:String, downloadPath:String):Void {
-        var host:Host = new Host(url);
+        trace(url);
+
+        var host:Host = new Host(url.trim());
         var gitSocket:Socket = new Socket();
 
         gitSocket.bind(host, 9418);
