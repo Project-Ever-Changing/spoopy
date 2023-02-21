9152e8712bc58e5ef6da0e5ea9a0b84e8f95e628
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-02-20 21:53:19 -0500

    callback point made

--- src/spoopy/obj/s3d/SpoopyMesh.hx
+++ src/spoopy/obj/s3d/SpoopyMesh.hx
@@ -12,8 +12,12 @@ class SpoopyMesh extends SpoopyPrimitive implements SpoopyNode3D {
     */
     public var rotationMode(default, set):SpoopyRotationMode;
 
-    public var size:SpoopyCallbackPoint;
-    public var position:SpoopyCallbackPoint;
+    public var size(default, null):SpoopyCallbackPoint;
+    public var position(default, null):SpoopyCallbackPoint;
+
+    public var angle_x(default, null):Float = 0;
+    public var angle_y(default, null):Float = 0;
+    public var angle_z(default, null):Float = 0;
 
     public function new(vPoints:Array<SpoopyPoint>, iPoints:Array<SpoopyPoint>) {
         super();
@@ -23,6 +27,26 @@ class SpoopyMesh extends SpoopyPrimitive implements SpoopyNode3D {
 
         size = new SpoopyCallbackPoint(1, 1, 1);
         position = new SpoopyCallbackPoint(0, 0, 0);
+
+        size.callback = __scale;
+        position.callback = __translate;
+        angle.callback = __rotation;
+    }
+
+    public function rotate(point:SpoopyPoint, angle:Float):Void {
+        
+    }
+
+    @:noCompletion function __scale(x:Float, y:Float, z:Float):Void {
+
+    }
+
+    @:noCompletion function __translate(x:Float, y:Float, z:Float):Void {
+
+    }
+
+    @:noCompletion function __rotation(x:Float, y:Float, z:Float):Void {
+
     }
 
     @:noCompletion function set_rotationMode(value:SpoopyRotationMode):SpoopyRotationMode {
--- src/spoopy/obj/s3d/SpoopyNode3D.hx
+++ src/spoopy/obj/s3d/SpoopyNode3D.hx
@@ -11,16 +11,6 @@ interface SpoopyNode3D {
     var rotationMode(default, set):SpoopyRotationMode;
 
     /*
-    * The 'scale' method determines the scaling factor for an object, which affects its size and proportion.
-    */
-    function scale(sx:Float, sy:Float, sz:Float):Void;
-
-    /*
-    * The `translate` method moves an object to a new location by changing its position coordinates.
-    */
-    function translate(dx:Float, dy:Float, dz:Float):Void;
-
-    /*
     * The `lookAt` method orients the object to face a given target by rotating it.
     */
     function lookAt(targetPos:SpoopyPoint, up:SpoopyPoint = new SpoopyPoint(0, 1, 0)):Void;
@@ -28,10 +18,5 @@ interface SpoopyNode3D {
     /*
     * The rotating methods rotate the object by the specified angle.
     */
-
-    function rotateX(angle:Float):Void;
-    function rotateY(angle:Float):Void;
-    function rotateZ(angle:Float):Void;
-
     function rotate(point:SpoopyPoint, angle:Float):Void;
 }
