b4c1cff3d2be982066215e97f503952348a5015f
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-04-10 06:19:40 -0400

    Update Main.hx

--- templates/project/source/Main.hx
+++ templates/project/source/Main.hx
@@ -5,11 +5,7 @@ import spoopy.app.SpoopyApplication;
 class Main extends SpoopyApplication {
     public function new() {
         super();
-    }
 
-    public override function onWindowCreate():Void {
-        
         // Code Goes Here
-
     }
 }
