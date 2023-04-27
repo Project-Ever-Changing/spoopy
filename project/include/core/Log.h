7fc65d664e0ba4eb20579ea30273942b8569f3f6
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-04-27 09:33:18 -0400

    Update Log.h

--- project/include/core/Log.h
+++ project/include/core/Log.h
@@ -6,22 +6,22 @@ namespace lime {
     class Log {
         public:
             static void Info(const char* message) {
-                printf("%s", "[WARN] ");
+                printf("%s%s%s", "\033[1m\033[37m", "[Info] ", "\033[0m");
                 printf("%s%s\n", "\033[0m", message);
             }
 
             static void Warn(const char* message) {
-                printf("%s", "[WARN] ");
+                printf("%s%s%s", "\033[1m\033[37m", "[WARN] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[33m", message, "\033[0m");
             }
 
             static void Error(const char* message) {
-                printf("%s", "[ERROR] ");
+                printf("%s%s%s", "\033[1m\033[37m", "[ERROR] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[31m", message, "\033[0m");
             }
 
             static void Success(const char* message) {
-                printf("%s", "[SUCCESS] ");
+                printf("%s%s%s", "\033[1m\033[37m", "[SUCCESS] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[32m", message, "\033[0m");
             }
     };
