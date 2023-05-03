849d8c02b087cbfeb67a0170458ad3762c34072a
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-05-03 11:11:32 -0400

    New logging

--- project/include/core/Log.h
+++ project/include/core/Log.h
@@ -24,6 +24,26 @@ namespace lime {
                 printf("%s%s%s", "\033[1m\033[37m", "[SUCCESS] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[32m", message, "\033[0m");
             }
+
+            static void Info(std::string message) {
+                printf("%s%s%s", "\033[1m\033[37m", "[Info] ", "\033[0m");
+                printf("%s%s\n", "\033[0m", message.c_str());
+            }
+
+            static void Warn(std::string message) {
+                printf("%s%s%s", "\033[1m\033[37m", "[WARN] ", "\033[0m");
+                printf("%s%s%s\n", "\033[1m\033[33m", message.c_str(), "\033[0m");
+            }
+
+            static void Error(std::string message) {
+                printf("%s%s%s", "\033[1m\033[37m", "[ERROR] ", "\033[0m");
+                printf("%s%s%s\n", "\033[1m\033[31m", message.c_str(), "\033[0m");
+            }
+
+            static void Success(std::string message) {
+                printf("%s%s%s", "\033[1m\033[37m", "[SUCCESS] ", "\033[0m");
+                printf("%s%s%s\n", "\033[1m\033[32m", message.c_str(), "\033[0m");
+            }
     };
 }
 
