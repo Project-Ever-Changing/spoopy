d760a173e431475d0e099c27ca68b5f137d1dedd
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-05-28 03:59:31 -0400

    Removed lime-project submodule

---
+++ project/src/device/Surface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Diego Fonseca on 5/25/23.
+//
+
+#include "Surface.h"
---
+++ project/src/device/Surface.h
@@ -0,0 +1,16 @@
+//
+// Created by Diego Fonseca on 5/25/23.
+//
+
+#ifndef PROJECT_SURFACE_H
+#define PROJECT_SURFACE_H
+
+
+
+class Surface {
+
+};
+
+
+
+#endif //PROJECT_SURFACE_H
