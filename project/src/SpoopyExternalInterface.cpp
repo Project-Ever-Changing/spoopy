fa26510393c297e4b5693bb5e09dc4cab423895c
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-05-30 23:00:40 -0400

    Update SpoopyExternalInterface.cpp

--- project/src/SpoopyExternalInterface.cpp
+++ project/src/SpoopyExternalInterface.cpp
@@ -2,11 +2,7 @@
 #include <string>
 #include <cstdlib>
 
-#include <graphics/Texture.h>
-#include <helpers/SpoopyBufferHelper.h>
 #include <system/CFFIPointer.h>
-#include <core/Log.h>
-
 #include <shaders/CrossShader.h>
 
 namespace lime {
