ee31c54f2a37c8b885130e11f1eecd99ac3a04c3
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-06-10 20:32:22 -0400

    Update GraphicsVulkan.cpp

--- project/src/graphics/vulkan/GraphicsVulkan.cpp
+++ project/src/graphics/vulkan/GraphicsVulkan.cpp
@@ -40,6 +40,8 @@ namespace lime { namespace spoopy {
     }
 
     GraphicsVulkan::~GraphicsVulkan() {
+        SPOOPY_LOG_INFO("About to be destroyed!");
+
         auto graphicsQueue = logicalDevice->GetGraphicsQueue();
         checkVulkan(vkQueueWaitIdle(graphicsQueue));
 
