200295fbbc403ad9e30c8866f5f0e97d61415d53
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-06-14 22:06:20 -0400

    Update SpoopyRenderPass.hx

--- src/spoopy/graphics/renderer/SpoopyRenderPass.hx
+++ src/spoopy/graphics/renderer/SpoopyRenderPass.hx
@@ -3,6 +3,8 @@ package spoopy.graphics.renderer;
 import spoopy.backend.native.SpoopyNativePass;
 import spoopy.graphics.SpoopyFormat;
 
+import lime.graphics.PixelFormat;
+
 class SpoopyRenderPass {
     @:noCompletion private var __attachments:Map<Int, SpoopyFormat>;
     @:noCompletion private var __backend:SpoopyRenderPass;
@@ -34,6 +36,17 @@ class SpoopyRenderPass {
 
         for(i in __colorCount...(__colorCount + __depthCount)) {
             __backend.addDepthAttachment(__attachments.get(i));
+        }
+    }
+
+    public static function getFormatFromPixelFormat(pixelFormat:PixelFormat):SpoopyFormat {
+        switch(pixelFormat) {
+            case BGRA32:
+                return VK_FORMAT_B8G8R8A8_UNORM;
+            case ARGB32:
+                return VK_FORMAT_A8B8G8R8_UNORM_PACK32;
+            default:
+                return VK_FORMAT_R8G8B8A8_UNORM;
         }
     }
 }
