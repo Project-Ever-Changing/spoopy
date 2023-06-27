67f747f6f47757b2bac3c83fffaba3f344056db0
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-06-27 08:29:14 -0400

    test

--- project/src/graphics/vulkan/GraphicsHandlerVulkan.cpp
+++ project/src/graphics/vulkan/GraphicsHandlerVulkan.cpp
@@ -6,6 +6,7 @@
 
 namespace lime { namespace spoopy {
     SDL_Context GraphicsHandler::CreateContext(SDL_Window* m_window) {
+        /*
         if(!GraphicsVulkan::Main) {
             GraphicsVulkan::Main = std::make_unique<GraphicsVulkan>(m_window);
         }
@@ -13,9 +14,11 @@ namespace lime { namespace spoopy {
         auto context = std::make_shared<ContextVulkan>();
         GraphicsVulkan::Main->contexts.push_back(context);
         return context;
+         */
     }
 
     void GraphicsHandler::DestroyContext(const SDL_Context &context) {
+        /*
         auto element = std::find_if(GraphicsVulkan::Main->contexts.begin(), GraphicsVulkan::Main->contexts.end(),
         [context](const SDL_Context &contextPtr){
             return contextPtr.get() == context.get();
@@ -31,9 +34,11 @@ namespace lime { namespace spoopy {
         if(GraphicsVulkan::Main->contexts.empty()) {
             GraphicsVulkan::Main.reset();
         }
+         */
     }
 
     int GraphicsHandler::MakeCurrent(SDL_Window* m_window, const SDL_Context &context) {
+        /*
         auto element = std::find_if(GraphicsVulkan::Main->contexts.begin(), GraphicsVulkan::Main->contexts.end(),
         [context](const SDL_Context &contextPtr){
             return contextPtr.get() == context.get();
@@ -50,17 +55,20 @@ namespace lime { namespace spoopy {
             *GraphicsVulkan::Main->logicalDevice,
             m_window
         );
+         */
 
         return 0;
     }
 
     int GraphicsHandler::SwapInterval(int vsync) {
+        /*
         auto& contexts = GraphicsVulkan::Main->contexts;
 
         for(size_t i=0; i<contexts.size(); ++i) {
             auto& context = contexts[i];
             context->SetVSYNC(vsync);
         }
+         */
 
         return 0;
     }
