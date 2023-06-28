a5749fe59d3f958ea952bef220592c9be0f71c85
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-06-27 20:38:52 -0400

    Update SCFrameBuffers.cpp

--- project/src/graphics/vulkan/framebuffers/SCFrameBuffers.cpp
+++ project/src/graphics/vulkan/framebuffers/SCFrameBuffers.cpp
@@ -6,22 +6,20 @@
 namespace lime { namespace spoopy {
     SCFrameBuffers::SCFrameBuffers(const LogicalDevice &device, const SwapchainVulkan &swapchain, const RenderPassVulkan &renderPass,
         const ImageDepth* depthImage, const Vector2T_u32 &extent) {
+        /*
         frameBuffers.resize(swapchain.GetImageCount());
 
         for(size_t i=0; i<frameBuffers.size(); i++) {
-            /*
             std::vector<VkImageView> attachments = {
                 swapchain.GetImageView(i)
             };
-             */
 
-            /*
             if(depthImage != nullptr) {
                 attachments.emplace_back(depthImage->GetView());
             }
 
             frameBuffers[i] = std::make_unique<FrameBufferVulkan>(device, extent, 1, attachments, renderPass.GetRenderpass());
-             */
+            */
         }
     }
 
