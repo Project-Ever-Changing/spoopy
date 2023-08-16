67efb8f154aed38045f4f798ba6183d1ed9e0101
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-08-16 01:04:56 -0400

    Update Log.h

--- project/include/core/Log.h
+++ project/include/core/Log.h
@@ -5,42 +5,42 @@
 namespace lime { namespace spoopy {
     class Log {
         public:
-            static void Info(const char* message) {
+            static inline void Info(const char* message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[Info] ", "\033[0m");
                 printf("%s%s\n", "\033[0m", message);
             }
 
-            static void Warn(const char* message) {
+            static inline void Warn(const char* message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[WARN] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[33m", message, "\033[0m");
             }
 
-            static void Error(const char* message) {
+            static inline void Error(const char* message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[ERROR] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[31m", message, "\033[0m");
             }
 
-            static void Success(const char* message) {
+            static inline void Success(const char* message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[SUCCESS] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[32m", message, "\033[0m");
             }
 
-            static void Info(std::string message) {
+            static inline void Info(std::string message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[Info] ", "\033[0m");
                 printf("%s%s\n", "\033[0m", message.c_str());
             }
 
-            static void Warn(std::string message) {
+            static inline void Warn(std::string message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[WARN] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[33m", message.c_str(), "\033[0m");
             }
 
-            static void Error(std::string message) {
+            static inline void Error(std::string message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[ERROR] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[31m", message.c_str(), "\033[0m");
             }
 
-            static void Success(std::string message) {
+            static inline void Success(std::string message) {
                 printf("%s%s%s", "\033[1m\033[37m", "[SUCCESS] ", "\033[0m");
                 printf("%s%s%s\n", "\033[1m\033[32m", message.c_str(), "\033[0m");
             }
