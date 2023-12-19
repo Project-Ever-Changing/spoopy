ba0335b76cf425c16690b49be5c3177d37e4870d
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-12-19 15:55:26 -0500

    Update CallbackPointer.cpp

--- project/src/system/CallbackPointer.cpp
+++ project/src/system/CallbackPointer.cpp
@@ -4,6 +4,7 @@
 namespace lime { namespace spoopy {
     void CallbackPointer::CallRaw() {
         if(IsCFFIValue()) {
+            SPOOPY_LOG_INFO("Run Thingy");
             CallRaw((hx::Object*)this->Get());
         }else {
             // Empty
@@ -11,8 +12,6 @@ namespace lime { namespace spoopy {
     }
 
     void* CallbackPointer::CallRaw(hx::Object* arg1) {
-        SPOOPY_LOG_INFO("Run Thingy");
-
         //hx::Throw(HX_NULL_FUNCTION_POINTER);
         return arg1->__run().GetPtr();
     }
