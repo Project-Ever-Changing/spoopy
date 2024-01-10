cb042378b600c8b36fb3da46e752a73b0b275091
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-01-09 23:30:10 -0500

    Update SpoopyExternalInterface.cpp

--- project/src/SpoopyExternalInterface.cpp
+++ project/src/SpoopyExternalInterface.cpp
@@ -71,8 +71,9 @@ namespace lime { namespace spoopy {
             SPOOPY_LOG_ERROR("Attempted to hook a callback to the swapchain recreate callback when one already exists!");
 
             SPOOPY_LOG_INFO(LOG_TYPE::FORMATTED, "Was the swapchain already initialized? %d. Was the recreate callback initialized? %d.",
-                context->GetSwapchain() != nullptr, context->coreRecreateSwapchain != nullptr
+                context->GetSwapchain() > 0, context->coreRecreateSwapchain > 0
             );
+
             return;
         }
 
